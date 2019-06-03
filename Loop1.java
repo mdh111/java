4b17b05f8fc50a97300e756b38f3fd3defec8e29
mdh111 <mikeyhill@gmail.com>
2019-06-03 11:02:38 +0100

    WHILE and FOR loops

---
+++ Loop1.java
@@ -0,0 +1,10 @@
+class Loop1 {
+    public static void main (String abc[]){
+        int a;
+        a=1;
+        while(a<=10){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
