b21268d93dfc09aadde4c3e047d25298a207ca66
mdh111 <mikeyhill@gmail.com>
2019-06-03 10:42:54 +0100

    first commit

---
+++ First.java
@@ -0,0 +1,11 @@
+class First {
+    public static void main (String abc[]){
+        int a;
+        int b;
+        int c;
+        a = 10;
+        b = 30;
+        c = a+b;
+        System.out.println("The result is: "+c);
+    }
+}
