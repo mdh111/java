154ecd7213e232c91bce1e8f34cf9a788eb2e514
mdh111 <mikeyhill@gmail.com>
2019-06-06 16:56:30 +0100

    Added remove duplicate function

--- StringProcessingTest.java
+++ StringProcessingTest.java
@@ -54,18 +54,59 @@ class StringProcessing{
         newMsg += word;
         System.out.println(newMsg);
     }
+    public boolean findWord(String msg, String word){
+        boolean found = false;
+        int i = 0;
+        String currentWord = "";
+        for(;i<msg.length();i++){
+            if(msg.substring(i,i+1).equals(" ")){
+                if(currentWord.equals(word)){
+                    found = true;
+                }
+                currentWord="";
+            }
+            else{
+                currentWord += msg.substring(i,i+1);
+            }
+        }
+        return found;
+    }
+    public void removeDuplicates(String msg){
+        int i = 0;
+        msg+=" ";
+        String word = "";
+        String newMsg = "";
+        for(;i<msg.length();i++){
+            if(msg.substring(i,i+1).equals(" ")){
+                if(findWord(newMsg,word)){
+                    //break;
+                }
+                else{
+                    newMsg += word + " ";
+                }
+                word="";
+            }
+            else{
+                word += msg.substring(i,i+1);
+            }
+        }
+        System.out.println(newMsg);
+    }
 }
+
+
 
 class StringProcessingTest{
     public static void main (String abc[]){
         StringProcessing sample = new StringProcessing();
-        sample.wordCount("Shafeeq is back");
-        System.out.println("-----------");
-        sample.printWords("hello my friends");
-        System.out.println("-----------");
-        sample.printWordsReverse("hello my friends");
-        System.out.println("-----------");
-        sample.printWordsReverseLine("hello my friends");
-        System.out.println("-----------");
+        //sample.wordCount("Shafeeq is back");
+        //System.out.println("-----------");
+        //sample.printWords("hello my friends");
+        //System.out.println("-----------");
+        //sample.printWordsReverse("hello my friends");
+        //System.out.println("-----------");
+        //sample.printWordsReverseLine("hello my friends");
+        //System.out.println("-----------");
+        sample.removeDuplicates("hello my friends hello friends my shafeeq");
     }
 }
