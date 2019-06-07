3c1c86265068b427686e62e36b8596f59f735623
mdh111 <mikeyhill@gmail.com>
2019-06-07 11:34:18 +0100

    Practicing with how variables work with exceptions

--- ExceptionsTest.java
+++ ExceptionsTest.java
@@ -1,12 +1,18 @@
 class Exceptions{
     public void divide(int a, int b){
             int c=0;
+            int d=3;
+            int f=0;
             try{
+                System.out.println("test");
+                f=9;
                 c=a/b;
                 System.out.println("Result: " + c);
             } 
             catch (ArithmeticException e){
                 System.out.println("Can't divide by zero");
+                System.out.println(d);
+                System.out.println(f);
             } 
     }
 }
