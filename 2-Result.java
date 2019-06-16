aa533086ffabc6ca6f0ad9ddd8fc685dc37244a0
mdh111 <mikeyhill@gmail.com>
2019-06-16 19:32:38 +0100

    (clean) Second java program

--- 2-Result.java
+++ 2-Result.java
@@ -1,15 +1,15 @@
 class Result {
     public static void main (String abc[]){
-        int phy,che,mat,total;
-        float per;
-        phy=100;
-        che=120;
-        mat=80;
-        total=phy+che+mat;
-        per=total*100/450;
-        System.out.println("Total marks: " + total);
-        System.out.println("Percentage: " + per);
-        if (per>=60){
+        int physics,chemistry,maths,total;
+        float percentage;
+        physics=100;
+        chemistry=120;
+        maths=80;
+        total=physics+chemistry+maths;
+        percentage=total*100/450;
+        System.out.println("Total marks (out of 450): " + total);
+        System.out.println("Percentage: " + percentage);
+        if (percentage>=60){
             System.out.println("Congratulations! You have passed!");
         }
         else{
