b9a780108191eb8f3159cacaa4c7d77f01573f5e
mdh111 <mikeyhill@gmail.com>
2019-06-16 19:40:28 +0100

    (clean) FOR loop. No initialisation or update

--- 5-Loop3.java
+++ 5-Loop3.java
@@ -1,10 +1,9 @@
 class Loop3 {
     public static void main (String abc[]){
-        int a;
-        a=10;
-        for(;a<=100;){
-            System.out.println(a);
-            a+=10;
+        int number=10;
+        for(;number<=100;){
+            System.out.println(number);
+            number+=10;
         }
     }
 }
