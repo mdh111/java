cee5f580806e19e32fbfc25b8f341475dafbde4f
mdh111 <mikeyhill@gmail.com>
2019-06-16 19:45:13 +0100

    (clean) First time creating an object from a class

--- 7-Test.java
+++ 7-Test.java
@@ -1,14 +1,16 @@
-class First{
-    int x;
-    int y;
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable.BinaryOp.Add;
+
+class Addition{
+    int number1;
+    int number2;
 }
 
 class Test{
     public static void main (String abc[]){
-        First ref;
-        ref = new First();
-        ref.x=10;
-        ref.y=20;
-        System.out.println("The result is: " + (ref.x + ref.y));
+        Addition add;
+        add = new Addition();
+        add.number1=10;
+        add.number2=20;
+        System.out.println("The result is: " + (add.number1 + add.number2));
     }
 }
